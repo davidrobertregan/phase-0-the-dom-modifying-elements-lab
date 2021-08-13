afe1ba6ebb67a40f81c8c841f6b4e4b3601bcbb8
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2021-08-13 08:48:42 -0500

    commit tree 86c98383118f08b4061c3351bd0a30cd6ccf98df

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 // Write your code here!
 
-var main = document.querySelector('main');
+// const main = document.querySelector('main');
+// main.remove();
+
+// const newHeader = document.createElement('h1');
+// newHeader.id = 'victory';
+// newHeader.innerHTML = "YOUR-NAME is the champion";
+
+// const element= document.createElement('div');
+
+// document.body.append(element);
+
+// const ul = document.createElement('ul');
+
+// for(let i = 0; i < 3; i++){
+//     const li = document.createElement('li');
+//     li.textContent = (i + 1).toString();
+//     ul.append(li);
+// }
+
+// element.append(ul);
+
+const main = document.querySelector('main')
+
 main.remove();
 
-var newHeader = document.createElement('h1');
-newHeader.id = 'victory';
-newHeader.innerHTML = "YOUR-NAME is the champion";
+const newHeader = document.createElement('h1')
+
+document.body.append(newHeader)
+
+newHeader.id = 'victory'
+
+newHeader.textContent = 'YOUR-NAME is the champion'
